68fb771b6eff72fcd3e54aa943f5db606fc8f697
Mike Vanoosthuyze <mike.vanoosthuyze@student.howest.be>
2022-11-21 22:07:32 +0100

    Repo GetCocktails

---
+++ ProjectDevice/ProjectDevice/ProjectDevice/Repositories/CocktailRepo.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using ProjectDevice.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDevice.Repositories
+{
+    public class CocktailRepo
+    {
+
+        public const string _BASEURL = "https://facocktails.azurewebsites.net/api/cocktails";
+
+        public static HttpClient GetHttpClient()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            return client;
+        }
+
+        public static async Task<List<Cocktail>> GetCocktails()
+        {
+            string url = $"{_BASEURL}";
+
+            using (HttpClient client = GetHttpClient())
+            {
+                try
+                {
+                    /*Opvragen van Api*/
+                    string json = await client.GetStringAsync(url);
+                    /* Hier doet newtonsoft zn werk */
+                    List<Cocktail> cocktail = JsonConvert.DeserializeObject<List<Cocktail>>(json);
+                    return cocktail;
+                }
+                catch (Exception ex)
+                {
+                    // ALWAYS add a breakpoint here
+                    throw ex;
+                }
+            }
+
+        }
+
+    }
+}
