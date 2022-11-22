2beab7b8b125db260506642e9d5594c0f399b2ca
Mike Vanoosthuyze <mike.vanoosthuyze@student.howest.be>
2022-11-22 18:24:52 +0100

    Test AddCocktail werkt

--- ProjectDevice/ProjectDevice/ProjectDevice/MainPage.xaml.cs
+++ ProjectDevice/ProjectDevice/ProjectDevice/MainPage.xaml.cs
@@ -5,10 +5,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ProjectDevice
 {
@@ -33,6 +36,31 @@ namespace ProjectDevice
             {
                 Debug.WriteLine(cocktail);
             }
+
+
+
+            /*Test AddCocktail */
+
+/*            Cocktail item = new Cocktail
+            {
+                Name = "PINA COLADA",
+                Alcoholic = true,
+                Instructions = "Doe de witte rum, de kokosmelk en het ananassap in de blender en voeg 2-3 eetlepels gemalen ijs toe. Meng alles glad. Schenk in een cocktailglas en serveer met een rietje. Versier de rand met een partje ananas.",
+                Image = "https://bettyskitchen.nl/wp-content/uploads/2016/05/klassieke_cocktails_pina_colada_shutterstock_420735802.jpg",
+                Ingredient1 = "25 ml white rum",
+                Ingredient2 = "50 ml thick coconut milk",
+                Ingredient3 = "1 dl pineapple juice",
+                Ingredient4 = "crushed ice",
+                Ingredient5 = "",
+                Ingredient6 = "",
+                Ingredient7 = "",
+                Ingredient8 = "",
+                Ingredient9 = "",
+                Ingredient10 = "",
+            };*/
+
+            /*await CocktailRepo.AddCocktail(item);*/
+
         }
     }
 }
